dd4e0562d7265b610bab5755426286b38d2224c6
Joseph <josephkeav95@hotmail.com>
2024-03-07 17:41:46 -0500

    added files in modules

---
+++ assignment1-N01551957/main.tf
@@ -0,0 +1,32 @@
+module "rgroup" {
+  source = "./modulules/rgroup-N01551957"
+}
+
+module "network" {
+  source = "./modules/network-N01551957"
+}
+
+module "linux" {
+  source = "./modules/vmlinux-N01551957"
+}
+
+module "windows" {
+  source = "./modules/vmwindows-N01551957"
+}
+
+module "common" {
+  source = "./modules/common-N01551957"
+}
+
+module "database" {
+  source = "./modules/database-N01551957"
+}
+
+module "datadisk" {
+  source = "./modules/datadisk-N01551957"
+}
+
+module "loadbalancer" {
+  source = "./modules/loadbalancer-N01551957"
+}
+
